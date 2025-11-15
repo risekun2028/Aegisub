7da4c24d9cb822fc003409b63d04640fc5703165
houssemriahi3 <houssem-riahi3@live.fr>
2025-11-15 21:56:59 +0100

    Update edit.cpp

--- src/command/edit.cpp
+++ src/command/edit.cpp
@@ -215,13 +215,6 @@ struct parsed_line {
 
 		AssDialogueBlockPlain *plain = nullptr;
 		AssDialogueBlockOverride *ovr = nullptr;
-
-		// If blockn is out of range (can happen on empty lines), insert at start
-		if (blockn < 0 || blockn >= (int)blocks.size()) {
-			orig_pos = 0;
-			blockn = -1;
-		}
-
 		while (blockn >= 0 && !plain && !ovr) {
 			AssDialogueBlock *block = blocks[blockn].get();
 			switch (block->GetType()) {
@@ -249,15 +242,12 @@ struct parsed_line {
 		std::string insert(tag + value);
 		int shift = insert.size();
 		if (plain || blockn < 0) {
-			// Clamp orig_pos to valid range to avoid substr out-of-range
-			int text_len = (int)line->Text.get().size();
-			if (orig_pos < 0) orig_pos = 0;
-			if (orig_pos > text_len) orig_pos = text_len;
 			line->Text = line->Text.get().substr(0, orig_pos) + "{" + insert + "}" + line->Text.get().substr(orig_pos);
 			shift += 2;
 			blocks = line->ParseTags();
 		}
-		else if (ovr) {
+		else {
+			// We've reached here, ovr cannot be null
 			std::string alt;
 			if (tag == "\\c") alt = "\\1c";
 			// Remove old of same
@@ -281,8 +271,6 @@ struct parsed_line {
 
 			line->UpdateText(blocks);
 		}
-		else
-			assert(false);
 
 		return shift;
 	}
@@ -555,7 +543,7 @@ struct edit_find_replace final : public Command {
 
 	void operator()(agi::Context *c) override {
 		c->videoController->Stop();
-		ShowSearchReplaceDialog(c, true);
+		DialogSearchReplace::Show(c, true);
 	}
 };
 
@@ -1273,6 +1261,7 @@ struct edit_insert_original final : public Command {
 
 		line->Text = line->Text.get().substr(0, sel_start) + c->initialLineState->GetInitialText() + line->Text.get().substr(sel_end);
 		c->ass->Commit(_("insert original"), AssFile::COMMIT_DIAG_TEXT, -1, line);
+		c->textSelectionController->SetSelection(sel_start, sel_start + c->initialLineState->GetInitialText().length());
 	}
 };
 
